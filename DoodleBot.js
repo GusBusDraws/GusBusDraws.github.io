f41de4e23b635d058d1af159cc0fa5b15cc19a48
cgusb <cgusbecker@gmail.com>
2023-10-25 21:51:40 -0400

    Change input to textarea

--- DoodleBot.js
+++ DoodleBot.js
@@ -1,7 +1,13 @@
 function onClick($this) {
     let input = document.getElementById('doodlebot-input').value;
     console.log(input);
-    let promptLabel = document.getElementById('doodlebot-label');
     // promptLabel.innerHTML = input.value;
-    promptLabel.innerHTML = "DoodleBot coming soon!";
+    if (input.length == 0) {
+        repsonse = "DoodleBot coming soon!";
+    } else {
+        response = input
+    }
+    console.log(response);
+    let promptLabel = document.getElementById('doodlebot-label');
+    promptLabel.innerHTML = response
 }
