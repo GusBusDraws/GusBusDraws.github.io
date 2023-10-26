43c8f6f8cfa3cc85f765a4f12bef5bbdb9648f09
cgusb <cgusbecker@gmail.com>
2023-10-26 09:27:57 -0400

    Rename onClick() -> generate() and remove arg

--- DoodleBot.js
+++ DoodleBot.js
@@ -1,11 +1,11 @@
-function onClick($this) {
+function generate() {
     let input = document.getElementById('doodlebot-input').value;
     console.log('input:', input);
     let response;
     if (input.length === 0) {
         response = "DoodleBot coming soon!";
     } else {
-        response = "DoodleBot coming soon!" + '<br>' + input
+        response = "DoodleBot coming soon!" + '<br/>' + input
     }
     console.log('response:', response);
     let promptLabel = document.getElementById('doodlebot-label');
